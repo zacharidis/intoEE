dd15b09f3c8f35f04c98d05c6d898c51a2d30b54
Georgios Zacharidis <zacharidis@icloud.com>
2023-06-14 08:39:10 -0700

    added models for creating user

--- src/main/java/entrypoint/UsersEntryPoint.java
+++ src/main/java/entrypoint/UsersEntryPoint.java
@@ -1,5 +1,6 @@
 package entrypoint;
 
+
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -13,9 +14,9 @@ public class UsersEntryPoint {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public CreateUserResponseModel createUser(CreateUserRequestModel requestModel) {
+    public UserProfileRest createUser(CreateUserRequestModel requestModel) {
 
-        CreateUserResponseModel returnValue = new CreateUserResponseModel();
+        UserProfileRest returnValue = new UserProfileRest();
         return returnValue;
 
     }
---
+++ src/main/java/model/UserProfileRest.java
@@ -0,0 +1,11 @@
+package model;
+
+public class UserProfileRest {
+    private String firstName ;
+    private String lastName;
+
+    private String email;
+
+    private String href ; // user profile
+    private String userId; // publicly shown ID/username
+}
