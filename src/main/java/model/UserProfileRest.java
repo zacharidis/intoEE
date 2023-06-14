5cbde28ab04ae076b2be8a560ee4c8854ff9dd7b
Georgios Zacharidis <zacharidis@icloud.com>
2023-06-14 08:40:11 -0700

    finished UserProfileREst class with getters and setters

--- src/main/java/model/UserProfileRest.java
+++ src/main/java/model/UserProfileRest.java
@@ -1,5 +1,8 @@
 package model;
 
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class UserProfileRest {
     private String firstName ;
     private String lastName;
@@ -8,4 +11,44 @@ public class UserProfileRest {
 
     private String href ; // user profile
     private String userId; // publicly shown ID/username
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
