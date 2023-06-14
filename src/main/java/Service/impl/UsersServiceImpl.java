fa061fac35345c1a32a8553ca815dd3218da65ab
Georgios Zacharidis <zacharidis@icloud.com>
2023-06-14 09:15:41 -0700

    Validation added , missing custom exception

--- src/main/java/Service/impl/UsersServiceImpl.java
+++ src/main/java/Service/impl/UsersServiceImpl.java
@@ -2,10 +2,36 @@ package Service.impl;
 
 import DTO.UserDTO;
 import Service.UsersService;
+import utils.UserProfileUtils;
 
 public class UsersServiceImpl implements UsersService {
+
+    UserProfileUtils userProfileUtils = new UserProfileUtils();
     public UserDTO createUser(UserDTO user) {
         UserDTO returnValue = new UserDTO();
+
+
+        //add the validations here
+         userProfileUtils.validateRequiredFields(user);
+        // check if the user exists
+
+        // create entity object
+
+        // generate secure public id
+
+        // generate salt
+
+        // generate secure password
+
+        // record the object into the database
+
+        // return the value back (profile)
+
+
+
+
+
+
         return returnValue;
     }
 }
---
+++ src/main/java/utils/UserProfileUtils.java
@@ -0,0 +1,25 @@
+package utils;
+
+import DTO.UserDTO;
+import org.springframework.core.env.MissingRequiredPropertiesException;
+
+public class UserProfileUtils {
+
+
+    public void validateRequiredFields(UserDTO userDTO) throws MissingRequiredPropertiesException {
+
+        if (userDTO.getFirstName()==null ||
+                userDTO.getFirstName().isEmpty() ||
+            userDTO.getLastName() == null ||
+            userDTO.getLastName().isEmpty()  ||
+            userDTO.getEmail() ==null ||  userDTO.getEmail().isEmpty()) {
+            throw  new MissingRequiredFieldException(ErrorMessage.MISSING_REQUIRED_FIELD.getErrorMessage());
+        }
+    }
+
+
+
+
+
+
+}
