5e102c2f39272f9e3a30e780c1559c07185f971b
Georgios Zacharidis <zacharidis@icloud.com>
2023-06-14 08:51:05 -0700

    Added UserDTO class

---
+++ src/main/java/DTO/UserDTO.java
@@ -0,0 +1,76 @@
+package DTO;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+    private  static  final long serialVersionID = 1L;
+    private Long Id; // DB userId;
+    private String firstName ;
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private String salt;
+    private String encryptedPassword;
+
+    private String userId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
